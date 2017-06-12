6e5704bef8195cb573a5b978b288d66b95ea6bc7
Erik Larson <elarsonwebco@gmail.com>
2017-06-12 14:22:44 -0700

    new frontend workflow

--- mainSOS/gulpfile.js
+++ mainSOS/gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
 gulp.task('sass', function () {
-    return gulp.src('ngApp/sass/main.scss')
-        .pipe(sass({ outputStyle: 'compressed' })).pipe(gulp.dest('ngApp/css'));
+  return gulp.src('ngApp/sass/main.min.scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .pipe(gulp.dest('ngApp/css'));
 });
+
 gulp.task('sass:watch', function () {
-    return gulp.watch('ngApp/sass/**/*.scss', ['sass']);
+  return gulp.watch('ngApp/sass/**/*.scss', ['sass']);
 });
